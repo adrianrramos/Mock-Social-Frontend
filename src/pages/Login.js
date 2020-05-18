75587d972d09ef5493bf66de22d25639bb627cd7
Adrian Ramos <ramosadrian56@gmail.com>
2020-05-18 11:10:42 -0700

    login component with .data of undefined bug

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,12 +2,110 @@ import React, { useState } from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import PropTypes from "prop-types";
 import Logo from "../images/piggy.webp";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 // MUI Components
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState({ errors: {} });
+
+  const classes = useStyles();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setLoading(true);
+
+    const userData = {
+      email,
+      password,
+    };
+    axios
+      .post("/login", userData)
+      .then(res => {
+        console.log(res.data);
+        setLoading(false);
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        setErrors(err.response.data);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <Grid container className={classes.form}>
+      <Grid item sm />
+      <Grid item sm>
+        <img
+          src={Logo}
+          alt="Social Pig Official Logo"
+          className={classes.imgLogo}
+        />
+        <Typography variant="h3" className={classes.pageTitle}>
+          Login
+        </Typography>
+        <form noValidate onSubmit={e => handleSubmit(e)}>
+          <TextField
+            id="email"
+            name="email"
+            type="email"
+            label="Email"
+            helperText={errors.email}
+            error={errors.email ? true : false}
+            className={classes.textFeild}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            fullWidth
+          />
+          <TextField
+            id="password"
+            name="password"
+            type="password"
+            label="Password"
+            helperText={errors.password}
+            error={errors.password ? true : false}
+            className={classes.textFeild}
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            fullWidth
+          />
+          {errors.general && (
+            <Typography variant="body2" className={classes.customError}>
+              {errors.general}
+            </Typography>
+          )}
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            disabled={loading}
+          >
+            Login
+            {loading && (
+              <CircularProgress className={classes.progress} size={30} />
+            )}
+          </Button>
+          <div>
+            <small>
+              Don't own an account? sign up <Link to="/signup">here</Link>
+            </small>
+          </div>
+        </form>
+      </Grid>
+      <Grid item sm />
+    </Grid>
+  );
+};
 
 const useStyles = makeStyles({
   form: {
@@ -24,65 +122,15 @@ const useStyles = makeStyles({
   textFeild: {
     margin: "10px auto",
   },
+  button: {
+    margin: "20px auto",
+    textAlign: "center",
+  },
+  customError: {
+    color: "red",
+  },
+  progress: {},
 });
-
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const classes = useStyles();
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    console.log("hi");
-  };
-
-  return (
-    <Grid container className={classes.form}>
-      <Grid item sm />
-      <Grid item sm>
-        <img
-          src={Logo}
-          alt="Social Pig Official Logo"
-          className={classes.imgLogo}
-        />
-        <Typography variant="h2" className={classes.pageTitle}>
-          Login
-        </Typography>
-        <form noValidate onSubmit={e => handleSubmit(e)}>
-          <TextField
-            id="email"
-            name="email"
-            type="email"
-            label="Email"
-            className={classes.textFeild}
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            fullWidth
-          />
-          <TextField
-            id="password"
-            name="password"
-            type="password"
-            label="Password"
-            className={classes.textFeild}
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            fullWidth
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            className={classes.button}
-          />
-        </form>
-      </Grid>
-      <Grid item sm />
-    </Grid>
-  );
-};
 
 Login.propTypes = {
   classes: PropTypes.object.isRequired,
