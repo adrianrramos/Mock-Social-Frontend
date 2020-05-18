6d09b5e77f78e894e2d5b9d1c3fb46b43472d3a7
Adrian Ramos <ramosadrian56@gmail.com>
2020-05-18 09:42:22 -0700

    login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import PropTypes from "prop-types";
 import Logo from "../images/piggy.webp";
@@ -7,20 +7,37 @@ import Logo from "../images/piggy.webp";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
   form: {
     textAlign: "center",
   },
-  pageTitle: {},
+  pageTitle: {
+    marginBottom: 10,
+  },
   imgLogo: {
-    width: 10,
+    width: 100,
     height: "auto",
+    margin: "30px auto",
+  },
+  textFeild: {
+    margin: "10px auto",
   },
 });
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
   const classes = useStyles();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log("hi");
+  };
+
   return (
     <Grid container className={classes.form}>
       <Grid item sm />
@@ -28,11 +45,39 @@ const Login = () => {
         <img
           src={Logo}
           alt="Social Pig Official Logo"
-          className={classes.imgLogoe}
+          className={classes.imgLogo}
         />
         <Typography variant="h2" className={classes.pageTitle}>
           Login
         </Typography>
+        <form noValidate onSubmit={e => handleSubmit(e)}>
+          <TextField
+            id="email"
+            name="email"
+            type="email"
+            label="Email"
+            className={classes.textFeild}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            fullWidth
+          />
+          <TextField
+            id="password"
+            name="password"
+            type="password"
+            label="Password"
+            className={classes.textFeild}
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            fullWidth
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          />
+        </form>
       </Grid>
       <Grid item sm />
     </Grid>
