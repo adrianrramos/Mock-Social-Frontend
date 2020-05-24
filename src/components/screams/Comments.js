a9252ab417a2d92e60d2ccc88bf06552e405db30
Adrian Ramos <ramosadrian56@gmail.com>
2020-05-23 22:51:53 -0700

    finish the comment component

--- src/components/screams/Comments.js
+++ src/components/screams/Comments.js
@@ -22,44 +22,45 @@ const Comments = ({ comments }) => {
   const classes = styles();
   return (
     <Grid container>
-      {comments.map((comment, index) => {
-        const { body, createdAt, userImage, userHandle } = comment;
-        return (
-          <Fragment>
-            <Grid item sm={12}>
-              <Grid container>
-                <Grid item sm={12}>
-                  <img
-                    scr={userImage}
-                    alt="user who commented"
-                    className={classes.commentImage}
-                  />
-                </Grid>
-                <Grid item sm={9}>
-                  <div classes={classes.commentData}>
-                    <Typography
-                      variant="h5"
-                      component={Link}
-                      to={`/users/${userHandle}`}
-                      color="primary"
-                    >
-                      {userHandle}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      {dayjs(createdAt).format("h:mm a, MMMM DD YYYY")}
-                    </Typography>
-                    <hr />
-                    <Typography variant="body1">{body}</Typography>
-                  </div>
+      {comments &&
+        comments.map((comment, index) => {
+          const { body, createdAt, userImage, userHandle } = comment;
+          return (
+            <Fragment>
+              <Grid item sm={12}>
+                <Grid container>
+                  <Grid item sm={12}>
+                    <img
+                      scr={userImage}
+                      alt="user who commented"
+                      className={classes.commentImage}
+                    />
+                  </Grid>
+                  <Grid item sm={9}>
+                    <div classes={classes.commentData}>
+                      <Typography
+                        variant="h5"
+                        component={Link}
+                        to={`/users/${userHandle}`}
+                        color="primary"
+                      >
+                        {userHandle}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary">
+                        {dayjs(createdAt).format("h:mm a, MMMM DD YYYY")}
+                      </Typography>
+                      <hr />
+                      <Typography variant="body1">{body}</Typography>
+                    </div>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-            {index !== comments.length - 1 && (
-              <hr className={classes.hrVisible} />
-            )}
-          </Fragment>
-        );
-      })}
+              {index !== comments.length - 1 && (
+                <hr className={classes.hrVisible} />
+              )}
+            </Fragment>
+          );
+        })}
     </Grid>
   );
 };
